d28b0b9fd8c6fd2d8284e4c60f81af04128a9758
AjiHidayat <hidayataji71@gmail.com>
2020-10-08 22:42:57 +0700

    Buat project react baru

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="container">
+        <h1>Form Pembelian Buah</h1>
+        <div class="content">
+          <div class="title-content">
+            <h4>Nama Pelanggan</h4>
+            <h4>Daftar Item</h4>
+          </div>
+          <div class="form">
+            <input id="text-input" type="text" />
+            <div class="check">
+              <input type="checkbox" />
+              <label>Semangka</label>
+            </div>
+            <div class="check">
+              <input type="checkbox" />
+              <label>Jeruk</label>
+            </div>
+            <div class="check">
+              <input type="checkbox" />
+              <label>Nanas</label>
+            </div>
+            <div class="check">
+              <input type="checkbox" />
+              <label>Salak</label>
+            </div>
+            <div class="check">
+              <input type="checkbox" />
+              <label>Anggur</label>
+            </div>
+          </div>
+        </div>
+        <div class="btn-submit">
+          <button class="btn-primary">Kirim</button>
+        </div>
+      </div>
     </div>
   );
 }
